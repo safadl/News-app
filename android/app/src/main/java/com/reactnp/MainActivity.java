0b25adc41e19006aa679d54b2565610da88103eb
safadl <safadeldoul@gmail.com>
2020-08-07 14:04:12 +0100

    commit everything

---
+++ android/app/src/main/java/com/reactnp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.reactnp;
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      // here    
+             //SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+      
+/*
+        int i;
+        for(i=0;i<1000;i++){ }
+          if(i>=999)
+         SplashScreen.hide(this);*/
+    }
+  @Override
+  protected String getMainComponentName() {
+    return "reactnp";
+  }
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+//import 'react-native-gesture-handler';
+import {AppRegistry} from 'react-native';
+import App from './App';
+//import Splash from './src/components/Splash'
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
